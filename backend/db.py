dafa68081be846838e54bbe47232f81fdd5b6a6d
Aleksei <aleksei@MacBook-Pro-Aleksei.local>
2025-06-05 20:43:07 +0300

    FS004: add docker, dotenv, postgresql

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,16 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
+load_dotenv()
 
-DATABASE_URL = "postgresql+asyncpg://alex:123@localhost/gamedb"
+db_user = os.getenv('POSTGRES_USER')
+db_password = os.getenv('POSTGRES_PASSWORD')
+db_name = os.getenv('POSTGRES_DB')
+
+
+DATABASE_URL = f"postgresql+asyncpg://{db_user}:{db_password}@localhost:5432/{db_name}"
 
 engine = create_async_engine(DATABASE_URL, pool_size=10, max_overflow=20, echo=True, future=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
